7fe2abfa19d3521f93717ee3da50d17243ff7c26
sali6000 <34247058+sali6000@users.noreply.github.com>
2024-08-25 15:17:37 +0200

    Update project-docker-config.ps1
    Init project with parameters values

--- project-docker-config.ps1
+++ project-docker-config.ps1
@@ -3,3 +3,10 @@ $mail = Read-Host "Please enter the mail will be used for git ex: jonathan.tom@g
 
 git config --global user.name $username
 git config --global user.email $mail
+
+$nameProject = Read-Host "Please enter the name of project ex: myProject, this name will be used too for DB like myProject_db, then press ENTER"
+
+symfony new $nameProject --webapp
+
+code ./$nameProject
+
